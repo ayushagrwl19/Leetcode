cad0796c3fb17c79aea0549aed69aa34a7e936c0
Ayush Agrawal <59306464+ayushagrwl19@users.noreply.github.com>
2022-09-09 22:57:29 +0530

    Time: 32 ms (20.67%), Space: 13.3 MB (53.69%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        vector<pair<int,int>> v;
+    void setZeroes(vector<vector<int>>& matrix){
         int n=matrix.size();
         int m=matrix[0].size();
+        vector<int>rows(n+1,0);
+        vector<int>columns(m+1,0);
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
                 if(matrix[i][j]==0)
-                    v.push_back({i,j});
+                {
+                    rows[i]=1;
+                    columns[j]=1;
+                }
             }
         }
-        for(auto p:v)
+         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                matrix[p.first][j]=0;
-            }
-            for(int i=0;i<n;i++)
-            {
-                matrix[i][p.second]=0;
+                if(rows[i]==1||columns[j]==1)
+                {
+                    matrix[i][j]=0;
+                }
             }
         }
     }
